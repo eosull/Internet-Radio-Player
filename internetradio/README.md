26e4f8430deaf675e3e682b0d9c87f97af94872c
eosull <osullivaneoin442@gmail.com>
2024-02-27 16:19:15 +0000

    style:add BS icons, style search element

--- internetradio/README.md
+++ internetradio/README.md
@@ -7,3 +7,9 @@ https://www.radio-browser.info/faq
 
 Pyradios library for communicating with radio-browser API:
 https://pypi.org/project/pyradios/
+
+- First step, figure out internet radio API and learn how to make requests to and from the server.
+- Create Django project and add in required libraries
+- Add front end interface to make API requests
+- Display responses on the site
+- Style site (search, audio player etc.)
--- internetradio/browse/views.py
+++ internetradio/browse/views.py
@@ -7,21 +7,32 @@ def index(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
+            searched = True
             rb = RadioBrowser()
             search_term = form.cleaned_data['search_term']
             search_result = rb.search(name=search_term, hidebroken=True)
 
             if not search_result:
-                return render(request, 'index.html', {'form': form})
+                context = {
+                    'form': form,
+                    'searched': searched
+                }
+                return render(request, 'index.html', context)
             else:
                 context = {
                     'form': form,
                     'search_result': search_result,
+                    'searched': searched
                 }
                 return render(request, 'index.html', context)
 
     else:
+        searched = False
         form = SearchForm()
-        return render(request, 'index.html', {'form': form})
+        context = {
+                    'form': form,
+                    'searched': searched
+                }
+        return render(request, 'index.html', context)
 
     return render(request, 'index.html')
