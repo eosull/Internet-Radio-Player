f5c8939a98a63d670f70a5fa46d246a445a29e17
eosull <osullivaneoin442@gmail.com>
2024-05-03 15:24:04 +0000

    style:update site colour scheme

--- internetradio/internetradio/settings.py
+++ internetradio/internetradio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$h!yty-=19lqrnoaat6w*7m5g5z9+=1bxiig9fjlp)z@fu-7(x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-eosull-internetradiopla-z04imlebsiy.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-eosull-internetradiopla-l07v6xef1e3.ws-eu110.gitpod.io']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-eosull-internetradiopla-z04imlebsiy.ws-eu108.gitpod.io']
 
